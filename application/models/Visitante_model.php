0b1dc82e916d19cbd237217eb39884c3eba2b705
StephanyArian <ignaciostephany127@gmail.com>
2024-10-03 21:52:52 -0400

    Actualizacion de codigo de Visitante_model.php

--- application/models/Visitante_model.php
+++ application/models/Visitante_model.php
@@ -15,6 +15,29 @@ class Visitante_model extends CI_Model {
         return $query->row_array();
     }
 
+    public function get_visitante_by_ci_nit($ci_nit) {
+        $query = $this->db->get_where('visitante', array('CiNit' => $ci_nit));
+        return $query->row_array();
+    }
+     
+    public function get_estadisticas_visitantes($fecha_inicio = null, $fecha_fin = null) {
+        $this->db->select('COUNT(DISTINCT v.idVisitante) as total_visitantes');
+        $this->db->select('SUM(dv.CantAdultoMayor) as total_adulto_mayor, SUM(dv.CantAdulto) as total_adulto, SUM(dv.CantInfante) as total_infante');
+        $this->db->select('h.Dia, COUNT(*) as visitas_por_dia');
+        $this->db->from('visitante v');
+        $this->db->join('venta ven', 'ven.idVisitante = v.idVisitante');
+        $this->db->join('detalleventa dv', 'dv.idVenta = ven.idVenta');
+        $this->db->join('tickets t', 't.idVenta = ven.idVenta');
+        $this->db->join('horarios h', 'h.idHorarios = t.idHorarios');
+        if ($fecha_inicio && $fecha_fin) {
+            $this->db->where('ven.FechaCreacion >=', $fecha_inicio);
+            $this->db->where('ven.FechaCreacion <=', $fecha_fin);
+        }
+        $this->db->group_by('h.Dia');
+        $query = $this->db->get();
+        return $query->result_array();
+    } 
+    
     public function insert_visitante($data) {
         $data['FechaCreacion'] = date('Y-m-d H:i:s');
         return $this->db->insert('visitante', $data);
