88a6b24c13b1ae4e4008a8d57259ad9e857b739d
StephanyArian <ignaciostephany127@gmail.com>
2024-10-29 15:41:47 -0400

    Implementacion y modificiacion para la liberia de QR

--- application/libraries/Ci_qrcode.php
+++ application/libraries/Ci_qrcode.php
@@ -1,19 +1,30 @@
 <?php
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-require_once APPPATH . 'third_party/phpqrcode/qrlib.php';
-
 class Ci_qrcode {
     public function generate($params) {
-        if (!isset($params['data'])) return false;
+        // Cargar la librería QR
+        require_once APPPATH . 'third_party/phpqrcode/qrlib.php';
         
-        $params['savename'] = FCPATH . 'assets/img/qr_' . time() . '.png';
-        $params['level'] = isset($params['level']) ? $params['level'] : 'H';
-        $params['size'] = isset($params['size']) ? $params['size'] : 10;
-        
-        QRcode::png($params['data'], $params['savename'], $params['level'], $params['size'], 2);
-
-        
-        return $params['savename'];
+        try {
+            // Asegurarse de que existe el directorio padre
+            $dir = dirname($params['savename']);
+            if (!file_exists($dir)) {
+                mkdir($dir, 0777, true);
+            }
+            
+            // Generar el código QR
+            QRcode::png(
+                $params['data'],
+                $params['savename'],
+                $params['level'],
+                $params['size']
+            );
+            
+            return file_exists($params['savename']);
+        } catch (Exception $e) {
+            log_message('error', 'Error generando QR: ' . $e->getMessage());
+            return false;
+        }
     }
 }
